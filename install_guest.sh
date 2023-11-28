c5e81ea38ad12c36737aa84791712876aa2a280a
Matias Ezequiel Vara Larsen <mvaralar@redhat.com>
2023-11-28 15:37:23 +0100

    Add install_guest.sh
    Signed-off-by: Matias Ezequiel Vara Larsen <mvaralar@redhat.com>

---
+++ install_guest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Use the first arg as the id for the guest
+sudo virt-install \
+	--connect qemu:///system \
+	--console pty,target_type=serial \
+	--name centosstream-9-"${1}" \
+	--cpu host \
+	--graphics none \
+	--ram 2048 \
+	--disk bus=virtio,path=centosstream-9-"${1}".qcow2 \
+	--vcpus 2 \
+	--os-variant centos-stream9 \
+	--network network=default,model=virtio \
+	--noautoconsole \
+	--noreboot \
+	--import
